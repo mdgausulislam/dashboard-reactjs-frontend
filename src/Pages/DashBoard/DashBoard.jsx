c180c53c883a7155d99d8b3c093145894b29f267
Md Gousul Islam Hemonto <gausulislam5@gmail.com>
2024-05-29 13:00:05 +0600

    make a react recharts and customization

--- src/Pages/DashBoard/DashBoard.jsx
+++ src/Pages/DashBoard/DashBoard.jsx
@@ -9,20 +9,52 @@ import Menu from '@mui/material/Menu';
 import MenuItem from '@mui/material/MenuItem';
 import { HiDotsVertical } from 'react-icons/hi';
 import { Button } from '@mui/material';
-import { Chart } from "react-google-charts";
+import { AreaChart, Area, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
 
-
-export const data = [
-    ["Year", "Sales", "Expenses"],
-    ["2013", 1000, 400],
-    ["2014", 1170, 460],
-    ["2015", 660, 1120],
-    ["2016", 1030, 540],
+const data = [
+    {
+        name: 'MON',
+        uv: 2000,
+        pv: 2400,
+        amt: 2400,
+    },
+    {
+        name: 'TUE',
+        uv: 3000,
+        pv: 1398,
+        amt: 2210,
+    },
+    {
+        name: 'WED',
+        uv: 2000,
+        pv: 9800,
+        amt: 2290,
+    },
+    {
+        name: 'THU',
+        uv: 2780,
+        pv: 3908,
+        amt: 2000,
+    },
+    {
+        name: 'FRI',
+        uv: 1890,
+        pv: 4800,
+        amt: 2181,
+    },
+    {
+        name: 'SAT',
+        uv: 2390,
+        pv: 3800,
+        amt: 2500,
+    },
+    {
+        name: 'SUN',
+        uv: 3490,
+        pv: 4300,
+        amt: 2100,
+    },
 ];
-
-export const options = {
-    'backgroundColor': 'transparent'
-};
 
 const DashBoard = () => {
     const [anchorEl, setAnchorEl] = useState(null);
@@ -50,10 +82,10 @@ const DashBoard = () => {
                     <div className="col-md-4 pl-0">
                         <div className="box graphBox">
                             <div className="d-flex align-items-center w-100 bottomEle">
-                                <h4 className="text-white mb-0 mt-0">
+                                <h4 className="text-white mb-0 mt-0 pl-5">
                                     Total Sales
                                 </h4>
-                                <div className="ml-auto">
+                                <div className="ml-auto pr-3">
                                     <Button className="ml-auto toggleIcon" onClick={handleClick}><HiDotsVertical /></Button>
                                     <Menu
                                         className='dropDown_menu'
@@ -85,24 +117,35 @@ const DashBoard = () => {
                                     </Menu>
                                 </div>
                             </div>
-                            <h3 className='text-white font-weight-bold '>$3,787,681.00</h3>
-                            <p>$3,578.90 in last month</p>
+                            <h3 className='text-white font-weight-bold pl-5 pt-3'>$3,787,681.00</h3>
+                            <p className='pl-5'>$3,578.90 in last month</p>
 
-                            <Chart
-                                chartType="AreaChart"
-                                width="100%"
-                                height="200px"
-                                data={data}
-                                options={options}
-                            />
+                            <div className='recharts' style={{ width: '100%', height: '225px' }}>
+                                <ResponsiveContainer>
+                                    <AreaChart
+                                        className='areaCharts'
+                                        data={data}
+                                        margin={{
+                                            top: 0,
+                                            right: 0,
+                                            left: 0,
+                                            bottom: 0,
+                                        }}
+                                    >
+                                        <CartesianGrid strokeDasharray="3 3" />
+                                        <Tooltip />
+                                        <Area type="monotone" dataKey="name" stroke="#8884d8" fill="#8884d8" />
+                                        <Area type="monotone" dataKey="uv" stroke="#8884d8" fill="#8884d8" />
+                                    </AreaChart>
+                                </ResponsiveContainer>
+                            </div>
                         </div>
                     </div>
                 </div>
-
                 <div className="card shadow border-0 p-3 mt-4">
                     <h3 className='hd'>Best Selling Products</h3>
 
-                    <div className="row">
+                    <div className="row cardFilters">
                         <div className="col">
                             <h4>Show By</h4>
                         </div>
